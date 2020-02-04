7b33d94c468a5bbb1a95bbb395b716462fa3ad6e
bessistemn1i <bessistemn1i@gmail.com>
2020-02-05 00:11:27 +0200

    start working on discount-slider

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,3 +1,14 @@
 import ToTop from "./modules/toTop";
+import { tns } from '../../node_modules/tiny-slider/src/tiny-slider.js';
 
 new ToTop();
+
+if(document.querySelector('.discount__slider')) {
+    new tns({
+        mode: "carousel",
+        container: ".slider-discount",
+        items: 2,
+        gutter: 55,
+        fixedWidth: 308
+    })
+}
