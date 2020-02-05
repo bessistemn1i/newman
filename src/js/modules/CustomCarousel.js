1edcd017a1a483a125cb9864967940e8cd0e6f86
bessistemn1i <eduard.zhulega@yandex.ru>
2020-02-05 11:47:04 +0200

    complete sytles on discount section

---
+++ src/js/modules/CustomCarousel.js
@@ -0,0 +1,116 @@
+class CustomCarousel {
+    constructor() {
+      this.carousel = document.querySelector('.discount__slider');
+      if (!this.carousel) {
+        return;
+      }
+  
+      this.VISIBLE_SLIDES = 2;
+      this.MAGIC_NUMBER = 360;
+      this.currentSlideNum = 0;
+  
+      this.carouselWrapper = this.carousel.querySelector('.slider-discount');
+      this.slides = this.carousel.querySelectorAll('.slider-discount__item');
+      this.slidesArray = [...this.slides];
+      this.maxSlideNum = this.slidesArray.length - 1;
+  
+      this.prevButton = document.querySelector('.discount__slider-btn--prev');
+      this.nextButton = document.querySelector('.discount__slider-btn--next');
+  
+      this.checkSliderOnResize();
+      
+      this.addEvents();
+    }
+  
+    checkSliderOnResize() {
+      if(window.innerWidth >= 1000) {
+        this.prepareSlides();
+        this.arrangeSlides(this.currentSlideNum);
+        this.slides.forEach((el, idx) => {
+          if(idx == 2) {
+            el.style.opacity = 0.1;
+          }
+        })
+      }
+  
+      if (window.innerWidth < 1000) {
+        this.carouselWrapper.style = '';
+        this.slides.forEach(slide => {
+          slide.style = '';
+          slide.style.left = '';
+          slide.style.opacity = '';
+  
+        })
+      }
+    }
+  
+    prepareSlides() {
+      this.carouselWrapper.style.height = '416px';
+      this.carouselWrapper.style.marginBottom = '60px';
+      this.carouselWrapper.style.marginRight = -this.MAGIC_NUMBER + 'px';
+  
+      this.slides.forEach(slide => {
+        slide.style.position = 'absolute';
+        slide.style.width = '338px';
+      });
+    }
+  
+    arrangeSlides(slideNum) {
+      this.slides.forEach((slide, index) => {
+        let position = (slideNum + index) % (this.slides.length);
+        slide.style.left = position * this.MAGIC_NUMBER + 'px';
+  
+        if (this.direction > 0 && position > 1) {
+            slide.style.opacity = '0';
+            setTimeout(() => {
+                slide.style.opacity = '0.1';
+            }, 150);
+        } else if (this.direction < 0 && position === 0) {
+          slide.style.opacity = '0';
+            setTimeout(() => {
+                slide.style.opacity = '0.1';
+            }, 150);
+        } else {
+          slide.style.opacity = '1';
+        }
+        // if (position === 0 || position === this.maxSlideNum ) {
+        //   // slide.style.visibility = 'hidden';
+        //   slide.style.opacity = '0.5';
+        // } else {
+        //   // slide.style.visibility = 'visible';
+        //   slide.style.opacity = '1';
+        // }
+      });
+    }
+  
+    setCurrentSlideNum(slideNum) {
+      if (slideNum < 0) {
+        this.direction = 1;
+        slideNum = this.maxSlideNum;
+      } else if (slideNum > this.maxSlideNum) {
+        this.direction = -1;
+        slideNum = 0;
+      } else {
+        this.direction = this.currentSlideNum - slideNum;
+      }
+  
+      this.currentSlideNum = slideNum;
+      this.arrangeSlides(this.currentSlideNum);
+    }
+  
+    addEvents() {
+      this.prevButton.addEventListener('click', () => {
+        this.setCurrentSlideNum(this.currentSlideNum + 1);
+      });
+  
+      this.nextButton.addEventListener('click', () => {
+        this.setCurrentSlideNum(this.currentSlideNum - 1);
+      });
+  
+      window.addEventListener('resize', () => (this.checkSliderOnResize()));
+    }
+  
+  }
+  
+  export default CustomCarousel;
+  
--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,9 +1,10 @@
 import ToTop from "./modules/toTop";
-import { tns } from '../../node_modules/tiny-slider/src/tiny-slider.js';
+//import { tns } from '../../node_modules/tiny-slider/src/tiny-slider.js';
+import CustomCarousel from "./modules/CustomCarousel";
 
 new ToTop();
 
-if(document.querySelector('.discount__slider')) {
+/* if(document.querySelector('.discount__slider')) {
     new tns({
         mode: "carousel",
         container: ".slider-discount",
@@ -11,4 +12,6 @@ if(document.querySelector('.discount__slider')) {
         gutter: 55,
         fixedWidth: 308
     })
-}
+} */
+
+new CustomCarousel();
